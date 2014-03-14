94e22bf8169b552e4bd0b9ab4debc4f13eb7a040
masroorhasan <masroor.hasan.n@gmail.com>
2014-03-14 15:05:16 -0400

    no error gbn - not working

--- gbn_simulator.cpp
+++ gbn_simulator.cpp
@@ -4,15 +4,70 @@
 
 #include <list>
 #include <vector>
+// #include <iterator>
 
 using namespace std;
 
-struct frame
+class Frame
 {
-	int type;		//0: pckt, 1: ack
-	int length;
-	int sn;
+	public:
+		Frame(int);
+		Frame(int,int,int);
+		Frame(int,int,int,double);
+		~Frame();
+
+		int get_frame_type() 			{ return type; }
+		int get_frame_size() 			{ return length; }
+		int get_frame_sn() 				{ return sn; }
+		double get_frame_timestamp() 	{ return timestamp; }
+
+		void set_timestamp(double);
+		void set_sn(int);
+	private:
+		int type;		//0: pckt, 1: ack
+		int length;
+		int sn;
+		double timestamp;
 };
+
+Frame::Frame(int type)
+: type(type)
+{
+
+}
+
+Frame::Frame(int type, int sn, int length)
+: type(type)
+, sn(sn)
+, length(length)
+{
+	//
+}
+
+Frame::Frame(int type, int sn, int length, double timestamp)
+: type(type)
+, sn(sn)
+, length(length)
+, timestamp(timestamp)
+
+{
+	// length = type == 0 ? pckt_header + pckt_length : pckt_header;
+}
+
+Frame::~Frame()
+{
+	//
+}
+
+void Frame::set_timestamp(double new_time)
+{
+	this->timestamp = new_time;
+}
+
+void Frame::set_sn(int new_sn)
+{
+	this->sn = new_sn;
+}
 
 class GBN_Simulator
 {
@@ -21,8 +76,8 @@ class GBN_Simulator
 		~GBN_Simulator();
 
 		double channel(int);
-		void receiver();
-		Event* send();		
+		void receiver(int);
+		Event* send(int);		
 
 		int get_s1n();
 		int get_rn();
@@ -33,8 +88,8 @@ class GBN_Simulator
 		void update_tc(double);
 		void update_pckt_T(int,double);
 		void update_nea(int);
-		void update_window(int);
-		void update_buffer(int);
+		int update_window(int,int);
+		void update_buffer(int,int);
 		bool check_expected_acks(int);
 
 		void print_buffer();
@@ -62,7 +117,7 @@ class GBN_Simulator
 
 		//buffer and window
 		// list<frame*> buffer; 		//M: to hold a single pckt
-		vector<frame*> buffer;
+		vector<Frame*> buffer;
 		vector<double> pckt_T;			//T: pckt transmission time
 		vector<int> next_expected_ack;	//set of expected rn's
 		int window_size;
@@ -71,8 +126,8 @@ class GBN_Simulator
 		int pckt_length; 	//l
 		int pckt_header;	//H
 
-		frame *data_frame;
-		frame *ack_frame;
+		// frame *data_frame;
+		// frame *ack_frame;
 		// int ctr; 
 };
 
@@ -89,28 +144,14 @@ GBN_Simulator::GBN_Simulator(int header, int length, double c, double tao, doubl
 	//receiver
 	next_expected_frame = 0;
 
-	// buffer.resize(window_size);
-	// pckt_T.resize(window_size);
-	// next_expected_ack.resize(window_size);
+	double time_sent = 0.0;
 	//populate buffer
-	// for(int i = 0; i < window_size; i++)
-	// {
-	// 	int sn = i % (window_size+1);
-	// 	frame *d_frame = new frame();
-	// 	d_frame->type = 0;
-	// 	d_frame->length = pckt_header + pckt_length;
-	// 	d_frame->sn = i % (window_size+1);
-	// 	buffer.insert(buffer.begin()+i, d_frame);
-	// 	// buffer.push_back(d_frame);
-	// 	// next_expected_ack.insert(next_expected_ack.begin()+i, 0);
-	// 	// next_expected_ack.push_back(0);
-	// }
-
-	frame *d_frame = new frame();
-	d_frame->type = 0;
-	d_frame->length = pckt_header + pckt_length;
-	d_frame->sn = 0 % (window_size+1);
-	buffer.insert(buffer.begin(), d_frame);
+	for(int i = 0; i < window_size; i++)
+	{
+		// time_sent += ((double)((pckt_header + pckt_length)*8.0) / (double)channel_capacity);
+		Frame *d_frame = new Frame(0, i%(window_size+1), (pckt_header+pckt_length));
+		buffer.push_back(d_frame);
+	}
 
 	cout << "buffer size: " << buffer.size() << endl;
 	srand(time(NULL));
@@ -128,63 +169,107 @@ double GBN_Simulator::get_tc()
 
 double GBN_Simulator::get_Ttc(int index)
 {
-	cout << "getting: T[" << index << "] = " << pckt_T.at(index) << endl;
-	return pckt_T.at(index);
+	// cout << "getting: T[" << index << "] = " << buffer.at(index)->get_frame_timestamp() << endl;
+	return buffer.at(index)->get_frame_timestamp();
 }
 
 int GBN_Simulator::get_s1n()
 {
-	return buffer.front()->sn;
+	return buffer.front()->get_frame_sn();
 }
 
 bool GBN_Simulator::check_expected_acks(int rn)
 {
 	// cout << "rn: " << rn << endl;
-	for(int i = 0; i < window_size; i++)
-	{
+	std::vector<int>::iterator it;
+	it = std::find(next_expected_ack.begin(), next_expected_ack.end(), rn);
 
-		if(rn == next_expected_ack.at(i))
-			return true;
-	}
-
+	if(it != next_expected_ack.end())
+		return true;
+	
 	return false;
 }
 
-void GBN_Simulator::update_window(int rn)
+int GBN_Simulator::update_window(int ctr, int rn)
 {
 	cout << "Before SHIFT" << endl;
-	cout << "SN[0] = " << buffer.at(0)->sn << endl;
-	cout << "T[0] = " << pckt_T.at(0) << endl;
+	cout << "SN[0] = " << buffer.at(0)->get_frame_sn() << endl;
+	// cout << "T[0] = " << pckt_T.at(0) << endl;
 
-	vector<frame*> tmp_buffer;
-
-	int shift_size = (rn - buffer.front()->sn) % (window_size+1);
-	shift_size = shift_size < 0 ? shift_size * -1 : shift_size;
-
-	print_buffer();
-	print_T();
-	//rotate vector
-	std::rotate(buffer.begin(), buffer.begin()+shift_size, buffer.end());
-	std::rotate(pckt_T.begin(), pckt_T.begin()+shift_size, pckt_T.end());
-
-	// buffer.erase(buffer.begin(), buffer.begin() + shift_size);
-	// pckt_T.erase(pckt_T.begin(), pckt_T.begin() + shift_size);
-	
+	vector<Frame*> tmp_buffer;
+	int sn_acked = (rn + get_s1n())%(window_size+1);
 
 	int itr = 0;
-	while(itr < shift_size)
+	for(int i=0; i < buffer.size(); i++)
 	{
-		buffer.pop_back();
-		pckt_T.pop_back();
-		itr++;
+		if(buffer.at(i)->get_frame_sn() == sn_acked)
+		{
+			itr = i;
+			itr++;
+			break;
+		}
 	}
 
-	cout << "SHIFTING SN and T by: " << shift_size << endl;
-	cout << "SN[0] = " << buffer.at(0)->sn << endl;
-	cout << "T[0] = " << pckt_T.at(0) << endl;
+	int new_index = 0;
+	for(int i = itr; i < window_size; i++)
+	{
+		tmp_buffer.insert(tmp_buffer.begin()+new_index, buffer.at(i));
+		new_index++;
+	}
 
+	buffer.clear();
+	buffer = tmp_buffer;
+
+	for(int i = buffer.size(); i < window_size; i++)
+	{
+		int sn = (buffer.at(i-1)->get_frame_sn()+1)%(window_size+1);
+		Frame *d_frame = new Frame(0, sn, (pckt_header+pckt_length));
+
+		buffer.push_back(d_frame);
+	}
+	
+	cout << "ctr: " << ctr << endl;
+	cout << "itr: " << itr << endl;
+	ctr = (ctr-itr);
+	ctr++;
+
+	ctr = ctr < 0 ? ctr+window_size : ctr;
+	// int shift_size = (rn - buffer.front()->get_frame_sn()) % (window_size+1);
+	// shift_size = shift_size < 0 ? shift_size * -1 : shift_size;
+
+	//rotate vector
+	// std::rotate(buffer.begin(), buffer.begin()+shift_size, buffer.end());
+	// std::rotate(pckt_T.begin(), pckt_T.begin()+shift_size, pckt_T.end());
+
+
+	// int itr = 0;
+	// while(itr < shift_size)
+	// {
+	// 	buffer.pop_back();
+	// 	// pckt_T.pop_back();
+	// 	itr++;
+	// }
+	// cout << "SHIFTING SN and T by: " << shift_size << endl;
+	cout << "SN[0] = " << buffer.at(0)->get_frame_sn() << endl;
+	// cout << "T[0] = " << pckt_T.at(0) << endl;
 	print_buffer();
-	print_T();
+	return ctr;
+}
+
+void GBN_Simulator::update_buffer(int index, int shift_size)
+{
+	// print_buffer();
+	cout << "index: " << index << endl; 
+
+	int frame_sn = (buffer.at(index-1)->get_frame_sn()+1)%(window_size+1);
+	Frame *data_frame = new Frame(0, frame_sn, (pckt_header+pckt_length));
+	
+	//set time_stamp
+	cout << "putting new pckt in buffer: SN[" << index << "] = " <<  frame_sn << endl;
+
+	// buffer.insert(buffer.begin()+index, data_frame);
+	buffer.push_back(data_frame);
+	print_buffer();
 }
 
 void GBN_Simulator::update_tc(double new_current_time)
@@ -197,37 +282,19 @@ void GBN_Simulator::update_pckt_T(int ctr, double new_time)
 {
 	//check if indexing has to be mod N+1
 	// cout << "here" << endl;
-	pckt_T.insert(pckt_T.begin()+ctr, new_time);
-	cout << "UPDATING pckt_T: T[" << ctr << "] = " << pckt_T.at(ctr) << endl;
+	buffer.at(ctr)->set_timestamp(new_time);
+	cout << "UPDATING pckt_T: T[" << ctr << "] = " << buffer.at(ctr)->get_frame_timestamp() << endl;
 }
 
 void GBN_Simulator::update_nea(int ctr)
 {
 	// cout << "SN[" << ctr << "] = " << buffer.at(ctr)->sn << endl;
-	
-	int expected_ack = (buffer.at(ctr)->sn+1)%(window_size+1);
-	next_expected_ack.insert(next_expected_ack.begin()+ctr, expected_ack);
-	// next_expected_ack.push_back(expected_ack);
+
+	int expected_ack = (buffer.at(ctr)->get_frame_sn()+1)%(window_size+1);
+	// next_expected_ack.insert(next_expected_ack.begin()+ctr, expected_ack);
+	next_expected_ack.push_back(expected_ack);
 	cout << "UPDATING nea: next_expected_ack[" << ctr << "] = " << next_expected_ack.at(ctr) << endl;
 
-}
-
-void GBN_Simulator::update_buffer(int index)
-{
-	// print_buffer();
-	cout << "index: " << index << endl; 
-
-	int frame_sn = (buffer.at(index-1)->sn+1)%(window_size+1);
-	data_frame = new frame();
-	data_frame->type = 0;
-	data_frame->length = pckt_header + pckt_length;
-	data_frame->sn = frame_sn;
-
-	cout << "putting new pckt in buffer: SN[" << index << "] = " <<  frame_sn << endl;
-
-	buffer.insert(buffer.begin()+index, data_frame);
-	// buffer.push_back(data_frame);
-	print_buffer();
 }
 
 double GBN_Simulator::channel(int length)
@@ -241,10 +308,10 @@ double GBN_Simulator::channel(int length)
 	pckt_lost = false;
 	// ack_lost = false;
 
-	if(length < pckt_header + pckt_length)
-		channel_time += ((double)pckt_header*8.0 / (double)channel_capacity);
-	else
-		channel_time += ((double)((pckt_header + pckt_length)*8.0) / (double)channel_capacity);
+	// if(length < pckt_header + pckt_length)
+	// 	channel_time += ((double)pckt_header*8.0 / (double)channel_capacity);
+	// else
+	// 	channel_time += ((double)((pckt_header + pckt_length)*8.0) / (double)channel_capacity);
 
 
 	int total_num_bits = (length) * 8;
@@ -287,55 +354,43 @@ double GBN_Simulator::channel(int length)
 	return channel_time;
 }
 
-void GBN_Simulator::receiver()
+void GBN_Simulator::receiver(int pckt_ctr)
 {
 	//generate ack frame
-	ack_frame = new frame();
-	ack_frame->type = 1; //ack
-	ack_frame->length = pckt_header;
+	Frame *ack_frame = new Frame(1);
+	// ack_frame->type = 1; //ack
+	// ack_frame->length = pckt_header;
 
 	//if error - send ack frame with current next_expected_frame
 	// cout << "sn: " << this->get_s1n() << endl;
 
 	if(frame_error == 1)
 	{		
-		ack_frame->sn = next_expected_frame;	//rn
+		ack_frame->set_sn(next_expected_frame);	//rn
 	} 
 	else
 	{
 		//check frame sn == next_expected_frame
-		for(int i = 0; i < window_size; i++)
+		if(buffer.at(pckt_ctr)->get_frame_sn() == next_expected_frame)
 		{
-			if(buffer.at(i)->sn == next_expected_frame)
-			{
-				cout << "nef: " << next_expected_frame << endl;
-				next_expected_frame += 1;
-				next_expected_frame %= window_size+1;
-				ack_frame->sn = next_expected_frame;	//rn	
-				cout << "updated nef (assigning rn): " << next_expected_frame << endl;
-				break;
-			}
+			// cout << "nef: " << next_expected_frame << endl;
+			next_expected_frame = (next_expected_frame+1)%(window_size+1);
+			ack_frame->set_sn(next_expected_frame);	//rn
+			// cout << "updated nef (assigning rn): " << next_expected_frame << endl;
+			// break;
 		}
-		
-		// if(this->get_s1n() == next_expected_frame) 
-		// {
-		// 	cout << "incrementing nef" << endl;
-		// 	next_expected_frame += 1;
-		// 	next_expected_frame %= window_size+1;
-		// 	ack_frame->sn = next_expected_frame;	//rn	
-		// }
 	}
 	
 	// current_time += ((double)pckt_header / (double)channel_capacity);
 }
 
-Event* GBN_Simulator::send()
+Event* GBN_Simulator::send(int pckt_ctr)
 {
 	//call forward channel 
 	double fc_time = channel(pckt_header + pckt_length);
 	
 	//receiver logic
-	receiver();
+	receiver(pckt_ctr);
 
 	//call reverse channel
 	double rc_time = channel(pckt_header);
@@ -360,10 +415,10 @@ void GBN_Simulator::print_buffer()
 	if(!buffer.empty())
 	{	
 		int i = 0;
-		for(std::vector<frame*>::iterator it = buffer.begin(); it != buffer.end(); ++it)
+		for(std::vector<Frame*>::iterator it = buffer.begin(); it != buffer.end(); ++it)
 		{
-			frame *f = *it;
-			cout << "SN[" << i << "] = " << f->sn << endl;
+			Frame *f = *it;
+			cout << "SN[" << i << "] = " << f->get_frame_sn() << endl;
 			i++;
 		}
 	}
